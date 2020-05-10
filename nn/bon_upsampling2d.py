cd6ea609b3d205e22a5488f5f7a321c230a6f8c2
Korneel van den Broek <korneelvdbroek@users.noreply.github.com>
2020-05-10 22:45:28 +0200

    * figured out the padding magic...

--- nn/bon_upsampling2d.py
+++ nn/bon_upsampling2d.py
@@ -5,23 +5,32 @@
 import tensorflow as tf
 
 
-def bon_upsampling2d(inputs, strides):
+def bon_upsampling2d(inputs, strides, kernel_shape):
   """
   2D bed-of-nails up-sampling. Combining this up-sampling with a conv2d layer is equivalent to conv2d_transpose
+  Output is padded such that:
+     conv2d(output, padding='VALID') = conv2d_transpose(inputs)
+  when one uses a reversed kernel (see tests)
 
   :param inputs:     4D input tensor (shape=[batch_size, height, width, in_channels])
   :param strides:    2-tuple with strides
   :return:           4D input tensor (shape=[batch_size, height * strides[0], width * strides[1], in_channels])
   """
+  pad_total = tuple(kernel_shape[i] - 1 for i in range(2))
+  padding_magic = [(1 - (pad_total[i] % 2)) * (1 - (strides[i] % 2)) - max(0, strides[i] - pad_total[i]) // 2 + (
+                   pad_total[i] - (pad_total[i] // 2)) for i in range(2)]
+
+  # bed-of-nails up-sampling
   s_x = inputs.shape
   x = tf.reshape(inputs, [-1, s_x[1], 1, s_x[2], 1, s_x[3]])
-
-  padding_height, padding_width = [[strides[i] - 1, 0]
-                                   for i in range(2)]
-  print('strides = {0} ==> padding = {1}, {2}'.format(strides, padding_height, padding_width))
-
-  paddings = tf.constant([[0, 0]]*2 + [padding_height] + [[0, 0]] + [padding_width] + [[0, 0]])
-  x = tf.pad(x, paddings)
+  padding_height, padding_width = [[min(0, padding_magic[i]) + strides[i] - 1 - (strides[i] // 2),
+                                    -min(0, padding_magic[i]) + strides[i] // 2] for i in range(2)]
+  bed_of_nails_paddings = tf.constant([[0, 0]]*2 + [padding_height] + [[0, 0]] + [padding_width] + [[0, 0]])
+  x = tf.pad(x, bed_of_nails_paddings)
   outputs = tf.reshape(x, [-1, s_x[1] * strides[0], s_x[2] * strides[1], s_x[3]])
+
+  # pad such that output can directly be use in conv2d(padding='VALID')
+  conv2d_paddings = [[0, 0]] + [[max(0, padding_magic[i]), - max(0, padding_magic[i]) + pad_total[i]] for i in range(2)] + [[0, 0]]
+  outputs = tf.pad(outputs, conv2d_paddings)
 
   return outputs
--- nn/tests/bon_upsampling2d.py
+++ nn/tests/bon_upsampling2d.py
@@ -2,57 +2,29 @@ import unittest
 import tensorflow as tf
 
 import nn
-from unittest.mock import MagicMock
 
 
 class TestQuadraticConv2D(unittest.TestCase):
-  @staticmethod
-  def get_layer_input():
-    layer_input = tf.constant([[1, 2, 3, 4, 5],
-                               [4, 5, 6, 7, 8],
-                               [7, 8, 9, 10, 11],
-                               [1, 2, 3, 4, 5],
-                               [2, 3, 4, 5, 6]])
-    layer_input = tf.cast(layer_input, dtype=tf.float32)
-    layer_input = tf.expand_dims(layer_input, axis=0)
-    layer_input = tf.expand_dims(layer_input, axis=-1)
-    return layer_input
-
   def test_equivalence_to_conv2d_transpose(self):
-    """Test """
-    inputs = self.get_layer_input()
+    """Test that conv2d(bon_upsampling2d()) = conv2d_transpose()"""
+    inputs = tf.random.uniform(shape=[1, 10, 23, 1], minval=-10., maxval=10., dtype=tf.float32)
 
-    kernel = tf.constant([[2, 3, 5, 1, 2, 3],
-                          [7, 11, 13, 11, 12, 13],
-                          [17, 19, 23, 21, 22, 23]])
-    kernel = tf.expand_dims(kernel, axis=2)
-    kernel = tf.expand_dims(kernel, axis=3)
-    kernel = tf.cast(kernel, dtype=tf.float32)
-    strides = (2, 1)
-    out_channels = kernel.shape[-1]
+    kernel = tf.random.uniform(shape=[6, 16, 1, 1], minval=-10., maxval=10., dtype=tf.float32)
 
-    # compute via conv2d(bon_upsampling2d())
-    upsampled = nn.bon_upsampling2d(inputs, strides)
-    tf.print('upsampled = ', upsampled[0, :, :, 0], summarize=100)
-    kernel_reverse = tf.reverse(kernel, tf.constant([0, 1]))
-    tf.print('upsampled.shape = ', upsampled.shape)
-    pad_total = tuple(max(kernel.shape[i] - 1, 0) for i in range(2))
-    paddings = [[0, 0]] + [[pad_total[i] - pad_total[i] // 2, (pad_total[i] // 2)] for i in range(2)] + [[0, 0]]
-    tf.print('computed padding', paddings)
-    tf.print('conv2d padding', nn._compute_padding(upsampled.shape, 1, kernel.shape[0:2], strides))
+    for stride_width in range(1, 20):
+      strides = (2, stride_width)
+      out_channels = kernel.shape[-1]
 
-    upsampled = tf.pad(upsampled, paddings)
-    outputs = tf.nn.conv2d(upsampled, kernel_reverse, strides=(1, 1), padding='VALID')
-    tf.print('outputs = ', outputs[0, :, :, 0], summarize=100)
-    tf.print('outputs shape =', outputs.shape)
+      # compute via conv2d(bon_upsampling2d())
+      upsampled = nn.bon_upsampling2d(inputs, strides, kernel.shape)
+      kernel_reverse = tf.reverse(kernel, tf.constant([0, 1]))
+      outputs = tf.nn.conv2d(upsampled, kernel_reverse, strides=(1, 1), padding='VALID')
 
-    # compute via conv2d_transpose()
-    in_channels = inputs.shape[0]
-    output_shape = tf.constant([in_channels, inputs.shape[1]*strides[0], inputs.shape[2]*strides[1], out_channels])
-    answer = tf.nn.conv2d_transpose(inputs, kernel, output_shape, strides, padding='SAME')
-    tf.print('answer = ', answer[0, :, :, 0], summarize=100)
+      # compute via conv2d_transpose()
+      output_shape = tf.constant([inputs.shape[0], inputs.shape[1]*strides[0], inputs.shape[2]*strides[1], out_channels])
+      answer = tf.nn.conv2d_transpose(inputs, kernel, output_shape, strides, padding='SAME')
 
-    self.assertGreaterEqual(1e-4, tf.reduce_max(tf.abs(outputs - answer)), "Should be zero")
+      self.assertGreaterEqual(1e-3, tf.reduce_max(tf.abs(outputs - answer)), "Should be zero")
 
 
 if __name__ == '__main__':
